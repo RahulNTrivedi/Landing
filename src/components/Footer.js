f41db23c4d2e7cda130b5d40e5e822ee7a8957b8
RahulNTrivedi <rahult0705@gmail.com>
2020-07-26 15:43:15 -0400

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ class  Footer extends React.Component {
     <div className="stuff">
       <div id="Contacts" className="footerbottom">
         <footer className="bottom">
-          <a target="_blank" rel="noopener noreferrer" className="circle" href="https://www.instagram.com/rahult0705"><i className="fab fa-instagram socials">Instagram</i></a>
-          <a target="_blank" rel="noopener noreferrer" className="circle" href="https://www.linkedin.com/in/rahultrivedi0705/">LinkedIn<i className="fab fa-linkedin socials"></i></a>
-          <a target="_blank" rel="noopener noreferrer" className="circle" href="mailto:rahulntriv@gmail.com"><i className="far fa-envelope socials">Email</i></a>
-          <a target="_blank" rel="noopener noreferrer" className="circle" href="https://github.com/RahulNTrivedi"><i className="fab fa-github socials" >GitHub</i></a>
+          <a target="_blank" rel="noopener noreferrer" className="circle" href="https://www.instagram.com/rahult0705"><i className="fab fa-instagram socials"></i></a>
+          <a target="_blank" rel="noopener noreferrer" className="circle" href="https://www.linkedin.com/in/rahultrivedi0705/"><i className="fab fa-linkedin socials"></i></a>
+          <a target="_blank" rel="noopener noreferrer" className="circle" href="mailto:rahulntriv@gmail.com"><i className="far fa-envelope socials"></i></a>
+          <a target="_blank" rel="noopener noreferrer" className="circle" href="https://github.com/RahulNTrivedi"><i className="fab fa-github socials"></i></a>
         </footer>
         <footer className="attribution">
           Spaceship Icon made by <a className="icon" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a className="icon" href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
